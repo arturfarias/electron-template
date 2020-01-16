964b76739495deb9d5ff78ee1b293537b6d0228e
arturfarias <arcashaid@gmail.com>
2020-01-16 15:31:23 -0300

    cli gendando paginas

--- .cli.js
+++ .cli.js
@@ -1,22 +1,72 @@
 const program = require('commander');
 const package = require('./package.json');
 const chalk = require('chalk');
+const fs = require('fs');
 
 program.version(package.version);
 
 commandList = ["page"];
+
+function makeDir(path){
+    if (!fs.existsSync(path)){
+        fs.mkdirSync(path);
+        return true;
+    }
+    else{
+        return false;
+    }
+};
+
+function makeFile(path,name,content){
+    fs.writeFile(`${path}/${name}`,`${content}`, (err) =>{
+      if(err){
+            return false
+        }
+        return true
+    });
+};
+
+
+
+function makePage(name){
+    let path = `${__dirname}/pages/${name}`
+    let template = `<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+        <meta http-equiv="X-Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+        <link rel="stylesheet" href="./${name}.css">
+        <title>Hello World!</title>
+      </head>
+      <body>
+    
+        <script src="./${name}.js"></script>
+      </body>
+    </html>`
+
+    if(makeDir(path)){
+        makeFile(path,`${name}.html`,template);
+        makeFile(path,`${name}.css`,"");
+        makeFile(path,`${name}.js`,"");
+        console.log(chalk.green(`page ${name} was successfully created`));
+        
+    } else{
+        console.log(chalk.red(`There is already a directory named with this name in the project!`));
+    }
+
+};
 
 program
     .command('command <option> <value>')
     .description('criar um novo diretorio de paginas')
     .action((option,value) => {
         if(!commandList.includes(option)){
-            console.log()
             console.log(`${chalk.red(`${option} It is not a valid command, use one of the following commands: ${chalk.blue(commandList)}`)}`);
         }
 
         if(option == commandList[0]){
-            console.log(chalk.green("pagina gerada algum dia!"));
+            makePage(value)
         }
     });
 
---
+++ teste.js
@@ -0,0 +1,14 @@
+`<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+    <meta http-equiv="X-Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+    <link rel="stylesheet" href="./${name}.css">
+    <title>Hello World!</title>
+  </head>
+  <body>
+
+    <script src="./${name}.js"></script>
+  </body>
+</html>`
